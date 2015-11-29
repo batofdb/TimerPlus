bc1df67e96c4eb8ab96aadf4433bf114a008bc66
Francis Bato <batofdb@gmail.com>
2015-11-28 22:23:16 -0800

    setup folders

---
+++ TimerPlus/TimerSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimerSetupViewController.h
+//  TimerPlus
+//
+//  Created by Francis Bato on 11/28/15.
+//  Copyright © 2015 FrancisBato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerSetupViewController : UIViewController
+
+@end
---
+++ TimerPlus/TimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimerViewController.h
+//  TimerPlus
+//
+//  Created by Francis Bato on 11/28/15.
+//  Copyright © 2015 FrancisBato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerViewController : UIViewController
+
+@end
