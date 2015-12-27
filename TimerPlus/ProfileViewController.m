73ae4f3a7da883e4aeca57783dd65aaa39775600
Francis Bato <batofdb@gmail.com>
2015-12-26 20:27:37 -0500

    added comments

--- TimerPlus/ProfileViewController.m
+++ TimerPlus/ProfileViewController.m
@@ -19,7 +19,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *fullnameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (strong, nonatomic) UIPageViewController *pageViewController;
-//@property (nonatomic) AppDelegate *delegate;
 @property (weak, nonatomic) IBOutlet UIView *tableHeaderView;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIView *backgroundView;
@@ -42,20 +41,15 @@
     self.offset_HeaderStop = 40.0; // At this offset the background stops its transformation
     self.offset_HeaderLabel = 30.0; // Offset of top layout guide to top of header label
 
-
-
     self.items = @[@"Full body", @"Abs", @"Leg Day", @"Arms", @"test1",@"test2",@"test3",@"test4",@"test5",@"test6",@"test7",@"test8",@"Full body", @"Abs", @"Leg Day", @"Arms", @"test1",@"test2",@"test3",@"test4",@"test5",@"test6",@"test7",@"test8",@"Full body", @"Abs", @"Leg Day", @"Arms", @"test1",@"test2",@"test3",@"test4",@"test5",@"test6",@"test7",@"test8"];
-    //Set height of header
 
+    // Set table bg to clear so that you can see the header_bg view
     self.tableView.backgroundColor = [UIColor clearColor];
 
-    NSLog(@"table view header height:%f",self.backgroundView.frame.size.height);
-
-
+    // Set padding of table view to the height of the header_bg view
     self.tableView.contentInset = UIEdgeInsetsMake(self.backgroundView.frame.size.height, 0, 0, 0);
 
-    //[self updateHeaderView];
-
+    // Add white border to profile view
     self.profileBorderView.layer.cornerRadius = 5.0;
     self.profileBorderView.clipsToBounds = YES;
     self.profileImageView.layer.cornerRadius = 5.0;
@@ -75,30 +69,32 @@
 
     [self.backgroundView insertSubview:headerImageView belowSubview:self.headerLabel];
 
-    //Create blue background
+    //Create blur background
     self.blurHeaderImageView = [[UIImageView alloc] initWithFrame:self.backgroundView.bounds];
+
+    //Used fxblurview here
     self.blurHeaderImageView.image = [[UIImage imageNamed:@"header_bg"] blurredImageWithRadius:10 iterations:20 tintColor:[UIColor clearColor]];
     [self.blurHeaderImageView setContentMode:UIViewContentModeScaleAspectFill];
     self.blurHeaderImageView.alpha = 0.0;
     [headerImageView insertSubview:self.blurHeaderImageView aboveSubview:self.headerLabel];
 
+    //Set clips to bounds so that image is confined to superview if not image will overlay on tableview
     self.backgroundView.clipsToBounds = YES;
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
 
     //Add 20 due to the -20 top layout constraint
+    // Offset is scroll content offset (margin), height of background, and 20
     CGFloat offset = scrollView.contentOffset.y + self.backgroundView.bounds.size.height + 20;
-
-    NSLog(@"offset: %f scroll offset:%f background:%f",offset, scrollView.contentOffset.y, self.backgroundView.bounds.size.height);
 
     CATransform3D profileTransform = CATransform3DIdentity;
     CATransform3D tableHeaderTransform = CATransform3DIdentity;
 
 
-    if (offset < 0) {
+    //Handle Pull Down
 
-        //Handle Pull Down
+    if (offset < 0) {
 #pragma mark - Pull Down
         //Create a scale factor that is proportional to the amount of scroll offset
         CGFloat tableHeaderScaleFactor = -(offset)/self.backgroundView.bounds.size.height;
@@ -118,35 +114,36 @@
     } else {
 
         //Handle scroll up and down
+
 #pragma mark - Scroll Up/Down
 
         //Background transform
         tableHeaderTransform = CATransform3DTranslate(tableHeaderTransform, 0, MAX(-self.offset_HeaderStop, -offset), 0);
 
 
-        // Header Label
+        //Header Label
         self.headerLabel.hidden = NO;
 
+        //Offset keeps track of current position add the following dimensions:
+        //full name label origin (y), background height, and the header stop
         CGFloat alignToNameLabel = -offset + self.fullnameLabel.frame.origin.y + self.backgroundView.frame.size.height + self.offset_HeaderStop;
-
-        NSLog(@"align offset:%f -- handle origin: %f",alignToNameLabel, self.headerLabel.frame.origin.y);
-        NSLog(@"bg height:%f -- offset stop: %f",self.backgroundView.frame.size.height,self.offset_HeaderStop);
-        NSLog(@"offset %f",offset);
 
         CGRect newFrame = self.headerLabel.frame;
 
+        //Update the header label origin maxing out at the offsets up top
         newFrame.origin = CGPointMake(self.headerLabel.frame.origin.x, MAX(alignToNameLabel,self.offset_HeaderLabel + self.offset_HeaderStop));
-
         self.headerLabel.frame = newFrame;
 
-        // Blur
+        //Blur
+        //Visible when scrolled down
+        //Calculate ratio of current distance over the offset head label distance
         self.blurHeaderImageView.alpha = MIN(1.0,(offset - alignToNameLabel)/self.offset_HeaderLabel);
 
         //Profile transform
-
         CGFloat avatarScaleFactor = (MIN(self.offset_HeaderStop, offset)) / self.profileBorderView.bounds.size.height / 1.4; // Slow down the animation
         CGFloat avatarSizeVariation = ((self.profileBorderView.bounds.size.height * (1.0 + avatarScaleFactor)) - self.profileBorderView.bounds.size.height) / 2.0;
 
+        //x axis goes left on a negative value, y axis goes down on a positive
         profileTransform = CATransform3DTranslate(profileTransform, -avatarSizeVariation, avatarSizeVariation, 0);
         profileTransform = CATransform3DScale(profileTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0);
         
@@ -172,10 +169,8 @@
 
 
 
-    // Segment Transform
+    //Segment Transform
     CGFloat segmentViewOffset = self.tableHeaderView.frame.size.height - self.segmentView.frame.size.height - offset;
-
-
 
     CATransform3D segmentTransform = CATransform3DIdentity;
 
@@ -183,16 +178,9 @@
 
     self.segmentView.layer.transform = segmentTransform;
 
-
     // Set scroll view insets just underneath the segment control
     self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(CGRectGetMaxY(self.segmentView.frame), 0, 0, 0);
 
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:YES];
-
-    //[self updateHeaderView];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
